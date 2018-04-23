85403fe40755f8b8f1c6869bc728ec09f9dd9169
Madhur Garg <madhur@hipbar.com>
2018-04-23 19:55:37 +0530

    removed local state

--- src/about.js
+++ src/about.js
@@ -6,26 +6,20 @@ class AboutPage extends React.Component {
   constructor() {
     super()
     this.HomeComponent = <Fragment />
-    this.state = {
-      show: false
-    }
     this.showHome = this.showHome.bind(this)
   }
 
   showHome() {
     const Home = asyncComponent(() => import("./home").then(module => module.default),{ name: "Page 1" })
     this.HomeComponent = <Home />
-    this.setState({ show: true })
   }
 
   render() {
-    const { HomeComponent } = this
-    console.log(HomeComponent);
     return (
       <div>
         <h1>about</h1>
         <button onClick={this.showHome}>load home</button>
-        { HomeComponent }
+        { this.HomeComponent }
       </div>
     )
   }
